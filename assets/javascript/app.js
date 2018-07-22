8a3eca09f02f1afcc0071c2633ce72ab4e029ec3
tcknapp <trevor.knapp.1991@gmail.com>
2018-07-22 11:40:20 -0700

    updated JS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,27 @@
+$("#btn").on("click", function(event) {
+    event.preventDefault();
+    let formInput = $("#user").val();
+    let grabGif = $.get(
+      "https://api.giphy.com/v1/gifs/search?q=" +
+        formInput +
+        "&api_key=6CjfUAofGjvsi1tn9tiW0Cw4hixSKLFC&limit=1"
+    );
+    
+    let imgTagArray = $(".pic");
+  
+    grabGif.done(function(response) {
+      for (let i = 0; i < response.data.length; i++) {
+        let eachImageUrl = response.data[i].images.original.url;
+        // imageUrlArray.push(eachImageUrl); // push each image as you loop through the response array to the image array
+        imgTagArray[i].setAttribute("src", eachImageUrl); 
+      }
+    });
+  });
+  
+
+
+
+
 //For YOUTUBE 
 
 function tplawesome(e, t) {
@@ -12,9 +36,9 @@ function tplawesome(e, t) {
 }
 
 function addSound() {
-    $(".submit").on('click', function (e) {
+    $("#btn").on('click', function (e) {
         e.preventDefault();
-        var key = $('#input').val();
+        var key = $('#user').val();
         var query = "https://www.googleapis.com/youtube/v3/search?q=" + key + "&key=AIzaSyDj8nQkKtYUX1DyaGo6E43_gywHp4xWoSY" + "&part=snippet&mine=true&videoEmbeddable=true&type=video&" + "music";
 
         $.ajax({
@@ -55,104 +79,3 @@ function addSound() {
             });
         }
         addSound();
-
-
-
-
-
-/*$(document).ready(function () {
-
-    //array 
-    var topics = [];
-    console.log(topics);
-
-    //for showButtons
-    function showButtons() {
-        $("#topics-display").empty();
-
-        for (var i = 0; i < topics.length; i++) {
-            var newButton = $("<button>").text(topics[i]).attr("data-topic", topics[i]).addClass("topic-button");
-            $("#topics-display").append(newButton);
-            console.log("ha")
-        }
-    }
-
-
-        // for showGifs
-        function showGifs() {
-            $("#gifs-display").empty();
-
-            var topic = $(this).attr("data-topic");
-
-            //for Giphy Query
-            var queryURL = "http://api.giphy.com/v1/gifs/search?q="
-                + topic + "&rating=pg&api_key=6CjfUAofGjvsi1tn9tiW0Cw4hixSKLFC&limit=1";
-                
-
-            // AJAX GET request
-            $.ajax({
-                url: queryURL,
-                method: "GET"
-            })
-                .onLoad(function(response) {
-                    
-
-                    var results = response.data;
-
-                    // Grab the gifs
-
-                    for (var i = 0; i < results.length; i++) {
-
-                        var topicDiv = $("<div>").addClass("gif-div");
-
-                        var rating = results[i].rating;
-
-                        var still = results[i].images.fixed_height_still.url;
-
-                        var animate = results[i].images.fixed_height.url;
-
-                        var p = $("<p>").text("Rating: " + rating);
-
-                        var topicImage = $("<img>").addClass("gif").attr({"src": still, "data-still": still, "data-animate": animate, "data-state": "still"});
-
-                        topicDiv.append(topicImage);
-                        topicDiv.append(p);
-
-                        $("#gifs-display").prepend(topicDiv);
-                        console.log(topicDiv);
-
-                    }
-                });
-            }
-
-            // for animation
-        
-            function changeAnimate() {
-                var state = $(this).attr("data-state");
-            
-                if (state === "still") {
-                  $(this).attr("src", $(this).attr("data-animate"));
-                  $(this).attr("data-state", "animate");
-                } else {
-                  $(this).attr("src", $(this).attr("data-still"));
-                  $(this).attr("data-state", "still");
-                }
-              }
-            
-              //for "On clicks"
-              $(document).on("click", ".topic-button", showGifs);
-            
-              $(document).on("click", ".gif", changeAnimate);
-            
-              $("#add-topic").on("click", function(event) {
-                event.preventDefault();
-
-                var topic= $("#topic-input").val().trim();
-                $("#topic-input").val("");
-                topics.push(topic);
-                showButtons();
-              
-
-            
-            }); */
-              
